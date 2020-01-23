1409204f589aa01e941da9570db5a500ce6056a0
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-01-23 12:52:45 -0600

    pantalla definida

--- build-Restaurante-Desktop_Qt_5_12_2_MinGW_64_bit-Debug/ui_MainWindow.h
+++ build-Restaurante-Desktop_Qt_5_12_2_MinGW_64_bit-Debug/ui_MainWindow.h
@@ -11,8 +11,12 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStackedWidget>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +26,19 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QStackedWidget *stackedWidget;
+    QWidget *page;
+    QLineEdit *lineEdit;
+    QWidget *page_2;
+    QLabel *label;
+    QStackedWidget *stackedWidget_2;
+    QWidget *page_3;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QWidget *page_4;
+    QFrame *line;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -31,9 +48,52 @@ public:
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(900, 600);
         MainWindow->setMaximumSize(QSize(900, 600));
-        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(12, 12, 12);"));
+        MainWindow->setStyleSheet(QString::fromUtf8("/*background-color: rgb(12, 12, 12);\n"
+"color: rgb(255, 255, 255);*/"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
+        stackedWidget->setGeometry(QRect(10, -1, 881, 571));
+        stackedWidget->setMaximumSize(QSize(900, 600));
+        page = new QWidget();
+        page->setObjectName(QString::fromUtf8("page"));
+        lineEdit = new QLineEdit(page);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(90, 40, 113, 20));
+        stackedWidget->addWidget(page);
+        page_2 = new QWidget();
+        page_2->setObjectName(QString::fromUtf8("page_2"));
+        label = new QLabel(page_2);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(0, 20, 81, 21));
+        stackedWidget_2 = new QStackedWidget(page_2);
+        stackedWidget_2->setObjectName(QString::fromUtf8("stackedWidget_2"));
+        stackedWidget_2->setGeometry(QRect(90, 10, 780, 531));
+        page_3 = new QWidget();
+        page_3->setObjectName(QString::fromUtf8("page_3"));
+        label_2 = new QLabel(page_3);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(170, 70, 101, 21));
+        label_3 = new QLabel(page_3);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(30, 70, 101, 21));
+        label_4 = new QLabel(page_3);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(310, 70, 101, 21));
+        label_5 = new QLabel(page_3);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(510, 70, 101, 21));
+        stackedWidget_2->addWidget(page_3);
+        page_4 = new QWidget();
+        page_4->setObjectName(QString::fromUtf8("page_4"));
+        stackedWidget_2->addWidget(page_4);
+        line = new QFrame(page_2);
+        line->setObjectName(QString::fromUtf8("line"));
+        line->setGeometry(QRect(80, 0, 3, 580));
+        line->setFrameShape(QFrame::VLine);
+        line->setFrameShadow(QFrame::Sunken);
+        stackedWidget->addWidget(page_2);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -45,12 +105,22 @@ public:
 
         retranslateUi(MainWindow);
 
+        stackedWidget->setCurrentIndex(1);
+        stackedWidget_2->setCurrentIndex(0);
+
+
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
 
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        lineEdit->setText(QApplication::translate("MainWindow", "mesas", nullptr));
+        label->setText(QApplication::translate("MainWindow", "barra de opciones", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "mesa", nullptr));
+        label_3->setText(QApplication::translate("MainWindow", "mesa", nullptr));
+        label_4->setText(QApplication::translate("MainWindow", "mesa", nullptr));
+        label_5->setText(QApplication::translate("MainWindow", " mesa", nullptr));
     } // retranslateUi
 
 };
