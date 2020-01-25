6484f338eec5b6434181a887d0dac8f56ea3a994
joaquin21olmos <53886210+joaquin21olmos@users.noreply.github.com>
2020-01-25 17:58:20 -0600

    entrega de interfaz
    primera parte, faltan los formularios etc

--- Restaurante/main.cpp
+++ Restaurante/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <Administrador/principal_administrador.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+   // MainWindow w;
+   // w.show();
+    principal_administrador w;
     w.show();
+
 
     return a.exec();
 }
