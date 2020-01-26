8188959552736118540fb622503a67f271b63c41
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-01-26 14:59:29 -0600

    creacion del menu

--- Restaurante/main.cpp
+++ Restaurante/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <Mesero/principal_mesero.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
+    //MainWindow w;
+    //w.show();
+   Principal_Mesero m;
+       m.show();
     return a.exec();
 }
