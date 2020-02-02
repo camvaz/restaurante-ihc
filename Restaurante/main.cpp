9e82f4e3d95244dc43aaef4092dc1b22f2d4de78
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-02-02 13:51:36 -0600

    comentarios

--- Restaurante/main.cpp
+++ Restaurante/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
-   Principal_Mesero m;
-       m.show();
+   //Principal_Mesero m;
+     //  m.show();
     return a.exec();
 }
