bfa5a3e9c0c5e73f13631f3f2544eb2b17ff1653
joaquin21olmos <53886210+joaquin21olmos@users.noreply.github.com>
2020-02-01 21:28:47 -0600

    menu vinos y parrila ensalada mar y sopa

--- Restaurante/Mesero/principal_mesero.cpp
+++ Restaurante/Mesero/principal_mesero.cpp
@@ -1,5 +1,9 @@
 #include "principal_mesero.h"
 #include "ui_principal_mesero.h"
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QDebug>
 
 Principal_Mesero::Principal_Mesero(QWidget *parent) :
     QMainWindow(parent),
@@ -8,12 +12,48 @@ Principal_Mesero::Principal_Mesero(QWidget *parent) :
     ui->setupUi(this);
     //METODO QUE CARGA LAS IMAGENES
     CargarImagenes();
+
+    QSqlDatabase database;
+
+       database= QSqlDatabase::addDatabase("QMYSQL");
+       database.setHostName("localhost");
+       database.setPort(3306);
+       database.setDatabaseName("restaurante");
+       database.setUserName("root");
+       database.setPassword("");
+       if(!database.open()){
+           qDebug()<<database.lastError().text();
+
+       }
+       else {
+           qDebug()<<"Base de datos conectada";
+       }
+
 }
 
 Principal_Mesero::~Principal_Mesero()
 {
+
     delete ui;
 }
+
+//Metodo para limpiar los layouts
+//usar cuando se crean objetos como en los menus
+void clearLayout(QLayout *layout) {
+    QLayoutItem *item;
+    while((item = layout->takeAt(0))) {
+        if (item->layout()) {
+            clearLayout(item->layout());
+            delete item->layout();
+        }
+        if (item->widget()) {
+            delete item->widget();
+        }
+        delete item;
+    }
+}
+
+
 
 //METODO QUE CARGA IMAGENES DEL MENU
 void Principal_Mesero::CargarImagenes(){
@@ -53,13 +93,401 @@ void Principal_Mesero::CargarImagenes(){
 
 
 //SLOT DE PLATILLOS
-void Principal_Mesero::on_parilla_clicked(){ui->paginas->setCurrentIndex(2);}
+void Principal_Mesero::on_parilla_clicked()
+{
+    clearLayout(ui->menuParrila1);
+    ui->paginas->setCurrentIndex(2);
 
-void Principal_Mesero::on_ensalada_clicked(){ui->paginas->setCurrentIndex(3);}
+    //joaquin codigo
 
-void Principal_Mesero::on_mar_clicked(){ui->paginas->setCurrentIndex(4);}
+    QString parrilla;
+    QSqlQuery parrilla1;
+    parrilla="select *from Platillos where idCategoriaMenu=2;";
+    parrilla1.exec(parrilla);
 
-void Principal_Mesero::on_sopa_clicked(){ui->paginas->setCurrentIndex(5);}
+    int Cr=0;
+
+    while(parrilla1.next())
+    {
+        //primera columna
+        QLabel *dishN=new QLabel;
+        dishN->setText(parrilla1.value(1).toString());
+        dishN->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP=new QLabel;
+        dishP->setText(parrilla1.value(2).toString());
+        dishP->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre=new QPushButton();
+        agre->setText("Agregar");
+        agre->setFixedSize(QSize(300,50));
+        //q->setStyleSheet("background-color: rgb(138,198,242)");
+       // QSignalMapper *mapper1=new QSignalMapper(this);
+        //connect(q,SIGNAL(clicked(bool)),mapper1,SLOT(map()));
+        //mapper1->setMapping(q,folio);
+        //connect(mapper1,SIGNAL(mapped(QString)),this,SLOT(PonerCitas(QString)));
+
+        ui->menuParrila1->addWidget(dishN,Cr,0,1,1);
+        ui->menuParrila1->addWidget(dishP,Cr+1,0,1,1);
+        ui->menuParrila1->addWidget(agre,Cr+2,0,1,1);
+
+        //espacio entre las columnas
+        QLabel *esp=new QLabel;
+
+        esp->setFixedSize(QSize(50,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *esp2=new QLabel;
+        esp2->setFixedSize(QSize(50,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QLabel *esp3=new QLabel();
+        esp3->setFixedSize(QSize(50,50));
+
+        ui->menuParrila1->addWidget(esp,Cr,1,1,1);
+        ui->menuParrila1->addWidget(esp2,Cr+1,1,1,1);
+        ui->menuParrila1->addWidget(esp3,Cr+2,1,1,1);
+
+
+
+        //seguna columna
+        if(!parrilla1.next())
+        {
+
+        }
+        else{
+        QLabel *dishN2=new QLabel;
+        dishN2->setText(parrilla1.value(1).toString());
+        dishN2->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP2=new QLabel;
+        dishP2->setText(parrilla1.value(2).toString());
+        dishP2->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre2=new QPushButton();
+        agre2->setText("Agregar");
+        agre2->setFixedSize(QSize(300,50));
+
+        ui->menuParrila1->addWidget(dishN2,Cr,2,1,1);
+        ui->menuParrila1->addWidget(dishP2,Cr+1,2,1,1);
+        ui->menuParrila1->addWidget(agre2,Cr+2,2,1,1);
+        Cr=Cr+3;
+           }
+
+    }
+
+}
+
+void Principal_Mesero::on_ensalada_clicked()
+{
+    clearLayout(ui->menuEnsalada);
+    ui->paginas->setCurrentIndex(3);
+
+    //joaquin codigo
+
+    QString ensalada;
+    QSqlQuery ensalada1;
+    ensalada="select *from Platillos where idCategoriaMenu=5;";
+    ensalada1.exec(ensalada);
+
+    int Cr=0;
+
+    while(ensalada1.next())
+    {
+        //primera columna
+        QLabel *dishN=new QLabel;
+        dishN->setText(ensalada1.value(1).toString());
+        dishN->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP=new QLabel;
+        dishP->setText(ensalada1.value(2).toString());
+        dishP->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre=new QPushButton();
+        agre->setText("Agregar");
+        agre->setFixedSize(QSize(300,50));
+        //q->setStyleSheet("background-color: rgb(138,198,242)");
+       // QSignalMapper *mapper1=new QSignalMapper(this);
+        //connect(q,SIGNAL(clicked(bool)),mapper1,SLOT(map()));
+        //mapper1->setMapping(q,folio);
+        //connect(mapper1,SIGNAL(mapped(QString)),this,SLOT(PonerCitas(QString)));
+
+        ui->menuEnsalada->addWidget(dishN,Cr,0,1,1);
+        ui->menuEnsalada->addWidget(dishP,Cr+1,0,1,1);
+        ui->menuEnsalada->addWidget(agre,Cr+2,0,1,1);
+
+        //espacio entre las columnas
+        QLabel *esp=new QLabel;
+
+        esp->setFixedSize(QSize(50,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *esp2=new QLabel;
+        esp2->setFixedSize(QSize(50,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QLabel *esp3=new QLabel();
+        esp3->setFixedSize(QSize(50,50));
+
+        ui->menuEnsalada->addWidget(esp,Cr,1,1,1);
+        ui->menuEnsalada->addWidget(esp2,Cr+1,1,1,1);
+        ui->menuEnsalada->addWidget(esp3,Cr+2,1,1,1);
+
+
+
+        //seguna columna
+        if(!ensalada1.next())
+        {
+
+        }
+        else {
+
+
+        QLabel *dishN2=new QLabel;
+        dishN2->setText(ensalada1.value(1).toString());
+        dishN2->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP2=new QLabel;
+        dishP2->setText(ensalada1.value(2).toString());
+        dishP2->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre2=new QPushButton();
+        agre2->setText("Agregar");
+        agre2->setFixedSize(QSize(300,50));
+
+        ui->menuEnsalada->addWidget(dishN2,Cr,2,1,1);
+        ui->menuEnsalada->addWidget(dishP2,Cr+1,2,1,1);
+        ui->menuEnsalada->addWidget(agre2,Cr+2,2,1,1);
+        Cr=Cr+3;
+        }
+    }
+
+
+}
+
+void Principal_Mesero::on_mar_clicked()
+{
+    clearLayout(ui->menuMar);
+    ui->paginas->setCurrentIndex(4);
+
+    //joaquin codigo
+
+    QString mar;
+    QSqlQuery mar1;
+    mar="select *from Platillos where idCategoriaMenu=3;";
+    mar1.exec(mar);
+
+    int Cr=0;
+
+    while(mar1.next())
+    {
+        //primera columna
+        QLabel *dishN=new QLabel;
+        dishN->setText(mar1.value(1).toString());
+        dishN->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP=new QLabel;
+        dishP->setText(mar1.value(2).toString());
+        dishP->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre=new QPushButton();
+        agre->setText("Agregar");
+        agre->setFixedSize(QSize(300,50));
+        //q->setStyleSheet("background-color: rgb(138,198,242)");
+       // QSignalMapper *mapper1=new QSignalMapper(this);
+        //connect(q,SIGNAL(clicked(bool)),mapper1,SLOT(map()));
+        //mapper1->setMapping(q,folio);
+        //connect(mapper1,SIGNAL(mapped(QString)),this,SLOT(PonerCitas(QString)));
+
+        ui->menuMar->addWidget(dishN,Cr,0,1,1);
+        ui->menuMar->addWidget(dishP,Cr+1,0,1,1);
+        ui->menuMar->addWidget(agre,Cr+2,0,1,1);
+
+        //espacio entre las columnas
+        QLabel *esp=new QLabel;
+
+        esp->setFixedSize(QSize(50,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *esp2=new QLabel;
+        esp2->setFixedSize(QSize(50,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QLabel *esp3=new QLabel();
+        esp3->setFixedSize(QSize(50,50));
+
+        ui->menuMar->addWidget(esp,Cr,1,1,1);
+        ui->menuMar->addWidget(esp2,Cr+1,1,1,1);
+        ui->menuMar->addWidget(esp3,Cr+2,1,1,1);
+
+
+
+        //seguna columna
+        if(!mar1.next())
+        {
+
+        }
+        else {
+
+
+        QLabel *dishN2=new QLabel;
+        dishN2->setText(mar1.value(1).toString());
+        dishN2->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP2=new QLabel;
+        dishP2->setText(mar1.value(2).toString());
+        dishP2->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre2=new QPushButton();
+        agre2->setText("Agregar");
+        agre2->setFixedSize(QSize(300,50));
+
+        ui->menuMar->addWidget(dishN2,Cr,2,1,1);
+        ui->menuMar->addWidget(dishP2,Cr+1,2,1,1);
+        ui->menuMar->addWidget(agre2,Cr+2,2,1,1);
+        Cr=Cr+3;
+
+        }
+    }
+
+}
+
+void Principal_Mesero::on_sopa_clicked()
+{
+    clearLayout(ui->menuSopa);
+    ui->paginas->setCurrentIndex(5);
+    //joaquin codigo
+
+    QString sopa;
+    QSqlQuery sopa1;
+    sopa="select *from Platillos where idCategoriaMenu=5;";
+    sopa1.exec(sopa);
+
+    int Cr=0;
+
+    while(sopa1.next())
+    {
+        //primera columna
+        QLabel *dishN=new QLabel;
+        dishN->setText(sopa1.value(1).toString());
+        dishN->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP=new QLabel;
+        dishP->setText(sopa1.value(2).toString());
+        dishP->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre=new QPushButton();
+        agre->setText("Agregar");
+        agre->setFixedSize(QSize(300,50));
+        //q->setStyleSheet("background-color: rgb(138,198,242)");
+       // QSignalMapper *mapper1=new QSignalMapper(this);
+        //connect(q,SIGNAL(clicked(bool)),mapper1,SLOT(map()));
+        //mapper1->setMapping(q,folio);
+        //connect(mapper1,SIGNAL(mapped(QString)),this,SLOT(PonerCitas(QString)));
+
+        ui->menuSopa->addWidget(dishN,Cr,0,1,1);
+        ui->menuSopa->addWidget(dishP,Cr+1,0,1,1);
+        ui->menuSopa->addWidget(agre,Cr+2,0,1,1);
+
+        //espacio entre las columnas
+        QLabel *esp=new QLabel;
+
+        esp->setFixedSize(QSize(50,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *esp2=new QLabel;
+        esp2->setFixedSize(QSize(50,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QLabel *esp3=new QLabel();
+        esp3->setFixedSize(QSize(50,50));
+
+        ui->menuSopa->addWidget(esp,Cr,1,1,1);
+        ui->menuSopa->addWidget(esp2,Cr+1,1,1,1);
+        ui->menuSopa->addWidget(esp3,Cr+2,1,1,1);
+
+
+
+        //seguna columna
+        if(!sopa1.next())
+        {
+
+        }
+        else {
+
+
+        QLabel *dishN2=new QLabel;
+        dishN2->setText(sopa1.value(1).toString());
+        dishN2->setFixedSize(QSize(300,25));
+        //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+        QLabel *dishP2=new QLabel;
+        dishP2->setText(sopa1.value(2).toString());
+        dishP2->setFixedSize(QSize(300,25));
+        //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+        QPushButton *agre2=new QPushButton();
+        agre2->setText("Agregar");
+        agre2->setFixedSize(QSize(300,50));
+
+        ui->menuSopa->addWidget(dishN2,Cr,2,1,1);
+        ui->menuSopa->addWidget(dishP2,Cr+1,2,1,1);
+        ui->menuSopa->addWidget(agre2,Cr+2,2,1,1);
+        Cr=Cr+3;
+
+        }
+    }
+}
 
 void Principal_Mesero::on_entrada_clicked(){ui->paginas->setCurrentIndex(6);}
 
@@ -71,14 +499,155 @@ void Principal_Mesero::on_infantil_clicked(){ui->paginas->setCurrentIndex(9);}
 
 void Principal_Mesero::on_coctel_clicked(){ui->paginas->setCurrentIndex(11);}
 
-void Principal_Mesero::on_vino_clicked(){ui->paginas->setCurrentIndex(12);}
+void Principal_Mesero::on_vino_clicked()
+{
+    ui->paginas->setCurrentIndex(12);
+    //joaquin codigo
+    clearLayout(ui->menuVinos);
+
+    QString origen;
+    QSqlQuery origen1;
+    origen="select OrigenVino from Vinos group by OrigenVino;";
+    origen1.exec(origen);
+    QString Norig;
+    int Cr=0;
+    while(origen1.next())
+    {
+        Norig=origen1.value(0).toString();
+        QString vino;
+
+        QSqlQuery vino1;
+        vino="select *from Vinos where idCategoriaMenu=10 and OrigenVino='"+Norig+"';";
+        vino1.exec(vino);
+
+
+
+
+        QLabel *Or=new QLabel;
+        Or->setText(Norig);
+        Or->setFixedSize(QSize(300,25));
+        Or->setStyleSheet("background-color: rgb(227,84,17)");
+        ui->menuVinos->addWidget(Or,Cr,0,1,1);
+       //ui->menuVinos->addWidget(Or,Cr,1,1,1);
+       //ui->menuVinos->addWidget(Or,Cr,2,1,1);
+            Cr=Cr+1;
+       while(vino1.next())
+        {
+            //primera columna
+
+           QLabel *dishN=new QLabel;
+            dishN->setText(vino1.value(1).toString());
+            dishN->setFixedSize(QSize(300,25));
+            //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+            QLabel *dishP=new QLabel;
+            dishP->setText(vino1.value(2).toString());
+            dishP->setFixedSize(QSize(300,25));
+            //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+            QPushButton *agre=new QPushButton();
+            agre->setText("Agregar");
+            agre->setFixedSize(QSize(300,50));
+            //agre->setStyleSheet("background-color: rgb(138,198,242)");
+           // QSignalMapper *mapper1=new QSignalMapper(this);
+            //connect(q,SIGNAL(clicked(bool)),mapper1,SLOT(map()));
+            //mapper1->setMapping(q,folio);
+            //connect(mapper1,SIGNAL(mapped(QString)),this,SLOT(PonerCitas(QString)));
+
+            ui->menuVinos->addWidget(dishN,Cr,0,1,1);
+            ui->menuVinos->addWidget(dishP,Cr+1,0,1,1);
+            ui->menuVinos->addWidget(agre,Cr+2,0,1,1);
+
+            //espacio entre las columnas
+            QLabel *esp=new QLabel;
+
+            esp->setFixedSize(QSize(50,25));
+            //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+            QLabel *esp2=new QLabel;
+            esp2->setFixedSize(QSize(50,25));
+            //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+            QLabel *esp3=new QLabel();
+            esp3->setFixedSize(QSize(50,50));
+
+            ui->menuVinos->addWidget(esp,Cr,1,1,1);
+            ui->menuVinos->addWidget(esp2,Cr+1,1,1,1);
+            ui->menuVinos->addWidget(esp3,Cr+2,1,1,1);
+
+
+
+            //seguna columna
+            if(!vino1.next())
+            {
+               // ui->menuVinos->addWidget(esp,Cr,2,1,1);
+            //    ui->menuVinos->addWidget(esp2,Cr+1,2,1,1);
+              //  ui->menuVinos->addWidget(esp3,Cr+2,2,1,1);
+             Cr=Cr+3;
+            }
+            else {
+
+
+            QLabel *dishN2=new QLabel;
+            dishN2->setText(vino1.value(1).toString());
+            dishN2->setFixedSize(QSize(300,25));
+            //fol->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+
+            QLabel *dishP2=new QLabel;
+            dishP2->setText(vino1.value(2).toString());
+            dishP2->setFixedSize(QSize(300,25));
+            //m->setStyleSheet("background-color: rgb("+rgb+")");
+
+
+            QPushButton *agre2=new QPushButton();
+            agre2->setText("Agregar");
+            agre2->setFixedSize(QSize(300,50));
+
+            ui->menuVinos->addWidget(dishN2,Cr,2,1,1);
+            ui->menuVinos->addWidget(dishP2,Cr+1,2,1,1);
+            ui->menuVinos->addWidget(agre2,Cr+2,2,1,1);
+            Cr=Cr+3;
+
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
 
 //SLOT DE MENU DE NAVEGACION
-void Principal_Mesero::on_comanda_clicked(){ui->paginas->setCurrentIndex(1);}
+void Principal_Mesero::on_comanda_clicked()
+{
+    //agrego joaquin
+    clearLayout(ui->menuParrila1);
+    clearLayout(ui->menuMar);
+    clearLayout(ui->menuSopa);
+    clearLayout(ui->menuEnsalada);
+    //agrego joaquin
+    ui->paginas->setCurrentIndex(1);
+}
 
 void Principal_Mesero::on_mesas_clicked(){ui->paginas->setCurrentIndex(0);}
 
-void Principal_Mesero::on_bebidas_clicked(){ui->paginas->setCurrentIndex(10);}
+void Principal_Mesero::on_bebidas_clicked()
+{
+    ui->paginas->setCurrentIndex(10);
+    //codigo agregado joaquin
+    clearLayout(ui->menuVinos);
+}
 
 
 
