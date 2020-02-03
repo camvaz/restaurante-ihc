30a2c380b7f92807f09443061bcafc74f383dd17
roberthlml <carlo-roberto50@hotmail.com>
2020-02-02 22:45:11 -0600

    Revert "Merge branch 'GUI_Administrador'"
    This reverts commit 91e65214462592bd723bb4712fa559dd849ba98e, reversing
    changes made to 079283f5920d584f98103f88e635af4a86ac4403.

--- Restaurante/main.cpp
+++ Restaurante/main.cpp
@@ -1,15 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <Administrador/principal_administrador.h>
-
+#include <Mesero/principal_mesero.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-   // MainWindow w;
-   // w.show();
-    principal_administrador w;
-    w.show();
-
-
+    //MainWindow w;
+    //w.show();
+   Principal_Mesero m;
+       m.show();
     return a.exec();
 }
